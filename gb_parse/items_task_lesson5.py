21812f81bf83548303518f21be69c3a13a43248f
a8820 <alex.sokolov505@gmail.com>
2021-03-10 16:50:07 +0300

    TASK_LESSON5

---
+++ gb_parse/items_task_lesson5.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class GbParseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class HHVacancyItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    salary = scrapy.Field()
+    skills = scrapy.Field()
+    company_url = scrapy.Field()
+
+
+class HHCompanyItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    web_site = scrapy.Field()
+    fields_of_activity = scrapy.Field()
